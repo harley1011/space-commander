292c4a5081294c1e3cecead6d940e35d8cc6bf29
spaceconcordia <spaceconcordia@mustang.(none)>
2013-05-11 23:16:24 -0400

    Added a test to parse strings from input

--- src/parser.cpp
+++ src/parser.cpp
@@ -1,5 +1,8 @@
 #include "parser.h"
 #include <cstddef>
+#include <string>
+
+using namespace std;
 
 bool Parser::IsValidInput(char* input) {
     if (input == NULL) { return false; }
@@ -15,6 +18,14 @@ CommandParam* Parser::ParseBytes(char* input) {
     CommandParam* result = new CommandParam();
     result->type = input[0];
     
+    switch(input[0]) {
+        case 1: {
+            result->fields    = new void*[200];
+            string* s         = new string((input+2), input[1]);        
+            result->fields[0] = (void*)s;
+        }
+
+    }
     if (input[0] == 2) {
         result->fields    = new void*[1];
         result->fields[0] = (void*)input[1];
--- tests/unit/parser-test.cpp
+++ tests/unit/parser-test.cpp
@@ -1,6 +1,10 @@
 #include "CppUTest/TestHarness.h"
 #include <cstring>
+#include <string>
 #include "parser.h"
+#include <iostream>
+
+using namespace std;
 
 static const int BUFFER_SIZE = 10;
 static char zeroOutBuffer[BUFFER_SIZE];
@@ -34,5 +38,26 @@ TEST(Parser, ParseBytes_ValidCommandType_ReturnCommandParamObject) {
     actual = (CommandParam*) Parser::ParseBytes(input);
     CHECK_EQUAL(expected.type,      actual->type);
     CHECK_EQUAL(expected.fields[0], actual->fields[0]);
+    delete actual;
+}
+
+TEST(Parser, ParseBytes_CommandWithStringField_ReturnsString) {
+    char input[BUFFER_SIZE];
+    input[0] = 1;
+    input[1] = 4;   //Size of the string, no need for \0
+    input[2] = 't';
+    input[3] = 'e';
+    input[4] = 's';
+    input[5] = 't';
+
+    CommandParam* actual  = Parser::ParseBytes(input);
+    string* actualString  = static_cast<string* >(actual->fields[0]);
+    string expectedString = "test";
+
+    CHECK_EQUAL(expectedString, *actualString);
+  
+
+    //TODO: change void** fields to vector<void *>, delete each element in the vector
+    delete actualString; 
     delete actual;
 }
