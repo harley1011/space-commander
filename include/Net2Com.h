56474bb7eefa309911e2b2acdf6198119a3a309b
Joseph Martineau <joseph-antoine@spaceconcordia.ca>
2013-06-30 17:09:13 -0400

    add NamedPipe.h and Net2Com.h

---
+++ include/Net2Com.h
@@ -0,0 +1,33 @@
+#ifndef _NET2COM_H_
+#define _NET2COM_H_
+#include <NamedPipe.h>
+
+enum PIPE_NUM{
+    PIPE_ONE, PIPE_TWO, PIPE_THREE, PIPE_FOUR
+};
+
+class Net2Com{
+    private :
+        static const int NUMBER_OF_PIPES = 4;
+        static const char* pipe_str[];
+         
+        NamedPipe* pipe[NUMBER_OF_PIPES];
+        
+        NamedPipe* dataPipe_w;
+        NamedPipe* dataPipe_r;
+        NamedPipe* infoPipe_w;
+        NamedPipe* infoPipe_r;
+
+    public :
+        Net2Com(PIPE_NUM dataw, PIPE_NUM datar, PIPE_NUM infow, PIPE_NUM infor);
+        ~Net2Com();
+        int WriteToDataPipe(const char* str);
+        char* ReadFromDataPipe(char* buffer);
+        int WriteToInfoPipe(const char* str);
+        char* ReadFromInfoPipe(char* buffer);
+
+    private :
+        bool Initialize();
+        bool CreatePipes();
+};
+#endif
