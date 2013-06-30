bc821c988bef6682463cb2ef54815bca3c46c5e8
Joseph Martineau <joseph-antoine@spaceconcordia.ca>
2013-06-30 18:00:02 -0400

    Comment, fix

--- tests/unit/stubs/FakeNetman.cpp
+++ tests/unit/stubs/FakeNetman.cpp
@@ -4,9 +4,10 @@ int main(){
     Net2Com* channel =  new Net2Com(PIPE_ONE, PIPE_TWO, PIPE_THREE, PIPE_FOUR);
     char ONE = 1;
     char TWO_FIFTY_TWO = 252;
-    channel->WriteToInfoPipe(one);
+    channel->WriteToInfoPipe(ONE);
     channel->WriteToDataPipe(TWO_FIFTY_TWO);
 
+    // TODO send command
 
     delete channel;
 
