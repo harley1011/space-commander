04486d834bcedaeb677fbf2d2ea63e946a1e7b83
Joseph Martineau <joseph-antoine@spaceconcordia.ca>
2013-06-30 17:58:12 -0400

    clean up

--- tests/unit/stubs/FakeNetman.cpp
+++ tests/unit/stubs/FakeNetman.cpp
@@ -1,19 +1,14 @@
 #include <Net2Com.h>
 
 int main(){
-    Net2Com* com_channel = new Net2Com(PIPE_ONE, PIPE_TWO, PIPE_THREE, PIPE_FOUR);
-    
-    unsigned char ONE = 1;
-    unsigned char TWO_FIFTY_TWO = 252
-
-    com_channel->WriteToInfoPipe(ONE);
-    com_channel->WriteToDataPipe(TWO_FIFTY_TWO);
-
-    // TODO send command ...
+    Net2Com* channel =  new Net2Com(PIPE_ONE, PIPE_TWO, PIPE_THREE, PIPE_FOUR);
+    char ONE = 1;
+    char TWO_FIFTY_TWO = 252;
+    channel->WriteToInfoPipe(one);
+    channel->WriteToDataPipe(TWO_FIFTY_TWO);
 
 
+    delete channel;
 
-
-    delete com_channel;
-
+    return 0;
 }
