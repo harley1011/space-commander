ee893d526eb6c111f96f096aa4f6ee08550661fb
Joseph-Antoine Martineau-Gagné <joseph-antoine@spaceconcordia.ca>
2013-07-02 19:30:50 -0400

    Added 3 test cases

--- tests/unit/Net2Com-test.cpp
+++ tests/unit/Net2Com-test.cpp
@@ -18,6 +18,7 @@
 TEST_GROUP(Net2ComTestGroup){
     Net2Com* netman;
     Net2Com* commander;
+    static const int BUFFER_SIZE = 50;
     void setup(){
         netman = new Net2Com(PIPE_ONE, PIPE_TWO, PIPE_THREE, PIPE_FOUR);
         commander = new Net2Com(PIPE_TWO, PIPE_ONE, PIPE_FOUR, PIPE_THREE);
@@ -29,7 +30,6 @@ TEST_GROUP(Net2ComTestGroup){
 
 };
 TEST(Net2ComTestGroup, WriteToDataPipe_ReturnsCorrectNumberOfBytesWritten){
-    int BUFFER_SIZE = 50;
     char buffer[BUFFER_SIZE];
     const char* data = "SomeUsefullData";
     int result;
@@ -47,11 +47,46 @@ TEST(Net2ComTestGroup, WriteToDataPipe_ReturnsCorrectNumberOfBytesWritten){
     }
 }
 TEST(Net2ComTestGroup, ReadFromDataPipe_ReturnsCorrectString){
-    FAIL("TODO");
+    char buffer[BUFFER_SIZE];
+    const char* data = "myCommand";
+    int result;
+    pid_t pid = fork();
+    if (pid == 0){
+        result = commander->WriteToDataPipe(data);
+        CHECK_EQUAL(strlen(data), result);
+        exit(0);
+    }else{
+        netman->ReadFromDataPipe(buffer);
+        STRCMP_EQUAL(data, buffer);
+    }
 }
 TEST(Net2ComTestGroup, WriteToInfoPipe_ReturnsCorrectNumberOfBytesWritten){
-    FAIL("TODO");
+    char buffer[BUFFER_SIZE];
+    const unsigned char byte = 252;
+    int result;
+    pid_t pid = fork();
+
+    if (pid == 0){
+        commander->ReadFromInfoPipe(buffer);
+        CHECK_EQUAL(byte, (unsigned char)buffer[0]);
+        exit(0);
+    }else{
+        result = netman->WriteToInfoPipe(byte);
+        CHECK_EQUAL(1, result);
+    }
 }
 TEST(Net2ComTestGroup, ReadFromInfoPipe_ReturnsCorrectByte){
-    FAIL("TODO");
+    char buffer[BUFFER_SIZE];
+    const unsigned char byte = 252;
+    int result;
+    pid_t pid = fork();
+
+    if (pid == 0){
+        result = commander->WriteToInfoPipe(byte);
+        CHECK_EQUAL(1, result);
+        exit(0);
+    }else{
+        netman->ReadFromInfoPipe(buffer);
+        CHECK_EQUAL(byte, (unsigned char)buffer[0]);
+    }
 }
