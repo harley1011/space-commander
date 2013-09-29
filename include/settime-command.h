fa39f1def3113e08632acb16068bdf0a1a38db40
spaceconcordia <spaceconcordia@mustang.(none)>
2013-09-29 14:16:39 -0400

    Change constructor from seconds to year/month/day etc

--- include/settime-command.h
+++ include/settime-command.h
@@ -9,27 +9,23 @@ using namespace std;
 
 class SetTimeCommand : public ICommand {
 public:
-    SetTimeCommand(time_t seconds) {
-        this->seconds  = seconds; 
-        time_t nowtime = seconds;
-
-        tm *nowtm = gmtime(&this->seconds);
-
-        year   = nowtm->tm_year;
-        month  = nowtm->tm_mon;
-        day    = nowtm->tm_mday;
-        hour   = nowtm->tm_hour;
-        minute = nowtm->tm_min;
-        second = nowtm->tm_sec;
+    SetTimeCommand(char year, char month, char day, char hour, char minute, char second) {
+        this.year    = year;
+        this.month   = month;
+        this.day     = day;
+        this.hour    = hour;
+        this.minute  = minute;
+        this.second  = second;
+        this.seconds = year * month * day * hour * minute * second;
     }
 
     char GetYearSince1900()  { return year; };
-    char GetMonth()  { return month; };
-    char GetDay()    { return day; };
-    char GetHour()   { return hour; };
-    char GetMinute() { return minute; };
-    char GetSecond() { return second; };
-
+    char GetMonth()     { return month; };
+    char GetDay()       { return day; };
+    char GetHour()      { return hour; };
+    char GetMinute()    { return minute; };
+    char GetSecond()    { return second; };
+    time_t GetSeconds() { return seconds; };
     void Execute() {};
 private:
     char year;
