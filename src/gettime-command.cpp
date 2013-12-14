6621ebd09a99e781eb65ffdb6c2649ba34de9441
spaceconcordia <spaceconcordia@mustang.(none)>
2013-12-14 14:18:48 -0500

    Changed gettime to return seconds since epoch as char*

--- src/gettime-command.cpp
+++ src/gettime-command.cpp
@@ -4,6 +4,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <string.h>
+#include <iostream>
+
 void* GetTimeCommand::Execute(){
     struct timeval tv;
 
@@ -14,6 +17,12 @@ void* GetTimeCommand::Execute(){
 
     time_t* rawtime = (time_t*)malloc(sizeof(time_t));
     *rawtime = tv.tv_sec;
+
+    std::cout << *rawtime << endl;    
+
+    char* result = (char*)malloc(sizeof(char) * 50);
+    memset(result, '\0', sizeof(char) * 50);
+    sprintf(result, "%lld", (long long)*rawtime);
     
-    return (void*)rawtime;
+    return (void*)result;
 }
