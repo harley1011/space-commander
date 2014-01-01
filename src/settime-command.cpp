fcfbcb4f3be21b6e55dc964826747a247757ef5e
Joseph Martineau <joseph-antoine@spaceconcordia.ca>
2013-12-31 19:23:37 -0500

    settime

---
+++ src/settime-command.cpp
@@ -0,0 +1,17 @@
+#include "settime-command.h"
+
+void SetTimeCommand::Execute(){
+    struct timeval tv;
+    
+    if(gettimeofday(&tv, 0) == -1){
+        printf ("Error! gettimeofday()\n");
+        exit(1);
+    }
+
+    tv.tv_sec = GetSeconds();   
+
+    if (settimeofday(&tv, 0) != 0){
+        perror ("Error! settimeofday()\n");
+        exit(1);
+    }
+}
