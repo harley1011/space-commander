db1a33b30f359928fcc9f888cdeb79247f73b863
Space Concordia <olivier@spaceconcordia.ca>
2014-04-03 18:02:09 -0400

    added test logs and test setup

--- tests/gtest/cmd-test.cpp
+++ tests/gtest/cmd-test.cpp
@@ -2,13 +2,23 @@
 #include "gtest/gtest.h"
 #include <command-factory.h>
 #include <Net2Com.h>
+#include <fileIO.h>
+
+const char* testfiles = "../stubs"; // sample log files, don't delete!
+const char* logs = "../test-logs";
 
 class commands_test : public ::testing::Test {
 	protected:
 	virtual void SetUp() {
-}
+    mkdir(logs, 0775);
+    CopyDirRecursively(testfiles, logs);
+  }
+
+  virtual void TearDown() {
+    DeleteDirectoryContent(logs);
+    remove(logs);
+  }
   char* set_time = "0";
-  char* get_time = "1";
   char* upload = "2";
   char* get_log = "3";
   char* reboot  = "4";
@@ -17,6 +27,7 @@ class commands_test : public ::testing::Test {
 
 //
 TEST_F(commands_test, getTimeCommand) {
+  char* get_time = "1";
   ICommand* command = CommandFactory::CreateCommand(get_time);
   bool command_error = false;
   if(command == NULL)
