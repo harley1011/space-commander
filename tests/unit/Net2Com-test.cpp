02a4647d1baa5ebf1e6acef52840d3deb043192a
Joseph-Antoine Martineau-Gagné <joseph-antoine@spaceconcordia.ca>
2014-04-05 09:39:54 -0400

    Fix Net2Com Unit tests... adapt to last commit(s)

--- tests/unit/Net2Com-test.cpp
+++ tests/unit/Net2Com-test.cpp
@@ -8,6 +8,8 @@
 #include <cstring>
 #include <unistd.h>
 #include <stdio.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
 
 //************************************************************
@@ -21,8 +23,8 @@ TEST_GROUP(Net2ComTestGroup){
     static const int BUFFER_SIZE = 50;
     static const int NULL_CHAR_LENGTH = 1;
     void setup(){
-        netman = new Net2Com(PIPE_ONE, PIPE_TWO, PIPE_THREE, PIPE_FOUR);
-        commander = new Net2Com(PIPE_TWO, PIPE_ONE, PIPE_FOUR, PIPE_THREE);
+        netman = new Net2Com(Dnet_w_com_r, Dcom_w_net_r,  Inet_w_com_r, Icom_w_net_r);
+        commander = new Net2Com(Dcom_w_net_r, Dnet_w_com_r, Icom_w_net_r, Inet_w_com_r);
     }
     void teardown(){
         if (netman != NULL){
@@ -43,18 +45,19 @@ TEST(Net2ComTestGroup, WriteToDataPipe_ReturnsCorrectNumberOfBytesWritten){
     pid_t pid;
     pid = fork();
     if (pid == 0){                                          // Child
+        result = netman->WriteToDataPipe(data);
+        #ifdef DEBUG
+        printf("strlen(data) : %d, result : %d\n", strlen(data) + NULL_CHAR_LENGTH, result);
+        #endif
+        CHECK_EQUAL(strlen(data) + NULL_CHAR_LENGTH, result);
+
+    }else{                                                  // Parent
         commander->ReadFromDataPipe(buffer, BUFFER_SIZE);
         #ifdef DEBUG
         printf("data : %s, buffer : %s\n", data, buffer);
         #endif
         STRCMP_EQUAL(data, buffer);
         exit(0);
-    }else{                                                  // Parent
-        result = netman->WriteToDataPipe(data);
-        #ifdef DEBUG
-        printf("strlen(data) : %d, result : %d\n", strlen(data) + NULL_CHAR_LENGTH, result);
-        #endif
-        CHECK_EQUAL(strlen(data) + NULL_CHAR_LENGTH, result);
     }
 }
 TEST(Net2ComTestGroup, ReadFromDataPipe_ReturnsCorrectString){
@@ -78,12 +81,12 @@ TEST(Net2ComTestGroup, WriteToInfoPipe_ReturnsCorrectNumberOfBytesWritten){
     pid_t pid = fork();
 
     if (pid == 0){
+        result = netman->WriteToInfoPipe(&byte, 1);
+        CHECK_EQUAL(1, result);
+    }else{
         commander->ReadFromInfoPipe(buffer, BUFFER_SIZE);
         CHECK_EQUAL(byte, (unsigned char)buffer[0]);
         exit(0);
-    }else{
-        result = netman->WriteToInfoPipe(&byte, 1);
-        CHECK_EQUAL(1, result);
     }
 }
 TEST(Net2ComTestGroup, ReadFromInfoPipe_ReturnsCorrectByte){
