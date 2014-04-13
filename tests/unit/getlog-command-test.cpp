c93005ada1cec4b58f8962704e9e04d69ed8621c
Joseph Martineau <joseph-antoine@spaceconcordia.ca>
2014-04-13 15:13:42 -0400

    getlog test initial commit

---
+++ tests/unit/getlog-command-test.cpp
@@ -0,0 +1,23 @@
+#include "CppUTest/TestHarness.h"
+#include "CppUTest/MemoryLeakDetectorMallocMacros.h"
+
+#include "SpaceDecl.h"
+#include "command-factory.h"
+#include "icommand.h"
+
+
+TEST_GROUP(GetLogTestGroup){
+    void setup(){
+
+    }
+    void teardown(){
+
+    }
+};
+
+
+
+TEST(GetLogTestGroup, justTryingStuff){
+
+
+}
