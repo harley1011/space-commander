434cfd39705d6c5851620dccb4a48b6c113ba250
Harley <harley.1011@gmail.com>
2014-09-18 23:30:05 -0400

    Added parseresult to reboot

--- src/reboot-command.cpp
+++ src/reboot-command.cpp
@@ -2,10 +2,58 @@
 #include <sys/reboot.h>
 #include <linux/reboot.h>
 #include <unistd.h>
+#include "subsystems.h"
+#include "commands.h"
+#include "shakespeare.h"
+#include <stdio.h>
+extern const char* s_cs1_subsystems[];
 
+/*+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+*
+* NAME : Execute
+*
+* PURPOSE : Reboots the device
+*
+* RETURNS : A buffer contaning the cmd number and cmd status
+* 
+*-----------------------------------------------------------------------------*/
 void* RebootCommand::Execute(){
-
+    char* result = (char*)malloc(sizeof(char) * REBOOT_RTN_SIZE); 
     reboot(LINUX_REBOOT_CMD_RESTART);
+    result[0] = REBOOT_CMD;
+    result[1] = CS1_SUCCESS;
+    return result;
+}
+/*+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+*
+* NAME : ParseResult
+*
+* PURPOSE : Parse the result buffer returned by the reboot function
+*
+* ARGUMENTS : result    : pointer to the result buffer
+*
+* RETURNS : struct InfoBytes* to STATIC memory
+* 
+*-----------------------------------------------------------------------------*/
+void* RebootCommand::ParseResult(const char * result)
+{
+    static struct InfoBytesReboot info_bytes = {0};
+    info_bytes.reboot_status = result[1];
 
-    return 0;
+    FILE* logfile;
+    logfile=Shakespeare::open_log("home/logs",s_cs1_subsystems[COMMANDER]);
+
+    char buffer[60];
+    
+    if(info_bytes.reboot_status == CS1_SUCCESS)
+        sprintf(buffer, "Reboot success.");
+    else
+        sprintf(buffer, "Reboot failure.");
+
+    if(logfile!=NULL) {
+        Shakespeare::log(logfile, Shakespeare::NOTICE,s_cs1_subsystems[COMMANDER], buffer);
+    }
+    
+    return (void*)&info_bytes;
+
 }
