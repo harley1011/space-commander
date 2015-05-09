968b8ef0775ff1a01021afa00a08ea407fcff6e6
CandyRedTele <vagrant@consat1>
2015-05-09 19:08:14 +0000

    [cleanup-2] settime src

--- src/common/settime-command.cpp
+++ src/common/settime-command.cpp
@@ -1,109 +1,126 @@
-#include "common/settime-command.h"
-#include <string.h>
 #include <cerrno>
-#include <sys/time.h>
+#include <linux/rtc.h>
+#include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/time.h>
 #include <time.h>
-#include <linux/rtc.h>
+
 #include <SpaceString.h>
 #include <shakespeare.h>
-#include "i2c-device.h"
+
 #include "common/commands.h"
-#include "SpaceDecl.h"
+#include "common/settime-command.h"
 #include "common/subsystems.h"
+#include "i2c-device.h"
+#include "SpaceDecl.h"
+
 /*+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-*
-* NAME : SetTimeCommand
-*
-* ARGUMENTS : time  : input - time to set 
-* 
-*-----------------------------------------------------------------------------*/
+ *
+ * NAME : SetTimeCommand
+ *
+ * ARGUMENTS : time  : input - time to set 
+ * 
+ *-----------------------------------------------------------------------------*/
 SetTimeCommand::SetTimeCommand(time_t time) {
     this->seconds = time;
     this->rtc_bus_number = -1;
 }
+
 /*+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-*
-* NAME : SetTimeCommand
-*
-* ARGUMENTS : time  : input - time to set 
-* 
-*-----------------------------------------------------------------------------*/
+ *
+ * NAME : SetTimeCommand
+ *
+ * ARGUMENTS : time  : input - time to set 
+ * 
+ *-----------------------------------------------------------------------------*/
 SetTimeCommand::SetTimeCommand(time_t time, char rtc_bus_number) {
     this->seconds = time;
     this->rtc_bus_number = rtc_bus_number;
 }
+
 /*+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-*
-* NAME : Execute
-*
-* PURPOSE : Sets the time of the device to 'time'
-*
-* RETURNS : A buffer contaning the cmd number, cmd status, and time set
-* 
-*-----------------------------------------------------------------------------*/
+ *
+ * NAME : Execute
+ *
+ * PURPOSE : Sets the time of the device to 'time'
+ *
+ * RETURNS : A buffer contaning the cmd number, cmd status, and time set
+ * 
+ *-----------------------------------------------------------------------------*/
 void* SetTimeCommand::Execute(size_t* pSize){
-    struct timeval tv;
-    char *result;
+    struct timeval tv = {0};
+    char *result = 0;
+
     result = (char*)malloc(sizeof(char) * (SETTIME_RTN_SIZE + CMD_HEAD_SIZE) );
     *pSize = SETTIME_RTN_SIZE + CMD_HEAD_SIZE;
-    result[0] = SETTIME_CMD;
-    result[1] = CS1_SUCCESS;
-    tv.tv_sec = GetSeconds();   
+    result[CMD_ID] = SETTIME_CMD;
+    result[CMD_STS] = CS1_SUCCESS;
+    tv.tv_sec = this->GetSeconds();   
     tv.tv_usec = 0;
-    memcpy(result+CMD_HEAD_SIZE, &tv.tv_sec, sizeof(time_t)); 
-    if (settimeofday(&tv, 0) != 0){
-        result[1] = CS1_FAILURE;
+    memcpy(result + CMD_HEAD_SIZE, &tv.tv_sec, sizeof(time_t)); 
+
+    if (settimeofday(&tv, 0) != 0) {
+        result[CMD_STS] = CS1_FAILURE;
         return (void*)result;        
     }
-    if (rtc_bus_number != EOF)
-    {
+
+    if (rtc_bus_number != EOF) {
         int rtc_bus_number_convert = (int)rtc_bus_number;
         struct tm * time_info = localtime(&tv.tv_sec);
-        struct rtc_time rt = { time_info->tm_sec, time_info->tm_min, time_info->tm_hour, time_info->tm_mday, time_info->tm_mon, time_info->tm_year, time_info->tm_wday,time_info->tm_yday,time_info->tm_isdst};
-        if (I2CDevice::I2CWriteToRTC(rt,rtc_bus_number_convert) == -1)
-            result[1] = CS1_FAILURE; 
-
-
+        struct rtc_time rt = {  time_info->tm_sec, 
+                                time_info->tm_min, 
+                                time_info->tm_hour, 
+                                time_info->tm_mday, 
+                                time_info->tm_mon, 
+                                time_info->tm_year, 
+                                time_info->tm_wday,
+                                time_info->tm_yday,
+                                time_info->tm_isdst
+                              };
+        if (I2CDevice::I2CWriteToRTC(rt,rtc_bus_number_convert) == -1) {
+            result[CMD_STS] = CS1_FAILURE; 
+        }
     }
+
     return (void*)result;
 }
+
 /*+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-*
-* NAME : ParseResult
-*
-* PURPOSE : Parse the result buffer returned by the execute function and logs it with shakespeare
-*
-* ARGUMENTS : result    : pointer to the result buffer
-*
-* RETURNS : struct InfoBytes* to STATIC memory
-* 
-*-----------------------------------------------------------------------------*/
-
-InfoBytes* SetTimeCommand::ParseResult(char *result)
-{
-
+ *
+ * NAME : ParseResult
+ *
+ * PURPOSE : Parse the result buffer returned by the execute function and logs it with shakespeare
+ *
+ * ARGUMENTS : result    : pointer to the result buffer
+ *
+ * RETURNS : struct InfoBytes* to STATIC memory
+ * 
+ *-----------------------------------------------------------------------------*/
+InfoBytes* SetTimeCommand::ParseResult(char *result) {
     static struct InfoBytesSetTime info_bytes;
-    if(!result || result[0] != SETTIME_CMD) {
+
+    if (!result || result[CMD_ID] != SETTIME_CMD) {
         Shakespeare::log(Shakespeare::ERROR,cs1_systems[CS1_COMMANDER],"Possible SetTime failure: Can't parse result");
         info_bytes.time_status = CS1_FAILURE;
+
         return &info_bytes;
     }
-    info_bytes.time_status = result[1];
+
+    info_bytes.time_status = result[CMD_STS];
     info_bytes.time_set = SpaceString::getTimet(result+CMD_HEAD_SIZE);
 
-    char buffer[100];
+    char buffer[CS1_MAX_LOG_ENTRY];
    
-    if(info_bytes.time_status == CS1_SUCCESS)
-    {
-       snprintf(buffer,100,"SetTime success: Time set to %u seconds since epoch",(unsigned)info_bytes.time_set); 
+    if (info_bytes.time_status == CS1_SUCCESS) {
+        snprintf(buffer, CS1_MAX_LOG_ENTRY,
+                    "SetTime success: Time set to %u seconds since epoch", (unsigned)info_bytes.time_set); 
         Shakespeare::log(Shakespeare::NOTICE, cs1_systems[CS1_COMMANDER], buffer);
+    } else {
+        snprintf(buffer, CS1_MAX_LOG_ENTRY,
+                    "SetTime failure: Time failed to set %u seconds since epoch", (unsigned)info_bytes.time_set);
+        Shakespeare::log(Shakespeare::ERROR, cs1_systems[CS1_COMMANDER], buffer);
     }
-    else
-    {
-       snprintf(buffer,100,"SetTime failure: Time failed to set %u seconds since epoch",(unsigned)info_bytes.time_set);
-       Shakespeare::log(Shakespeare::ERROR, cs1_systems[CS1_COMMANDER], buffer);
-   }
+
     return &info_bytes;
 }
